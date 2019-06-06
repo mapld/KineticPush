c8c10fbc47c15038e92492500d08570cb4f2ef32
Alex Rehnby-Martin <amartin@bitstew.com>
2019-06-05 19:29:45 -0700

    Initial commit: Add unity project

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Rigidbody2D rigidBody;
+
+    private Vector2 movementVector;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxis("Horizontal") * movementSpeed;
+
+        movementVector.y = Input.GetAxis("Vertical") * movementSpeed;
+
+        rigidBody.velocity = movementVector;
+    }
+}
