a6079225d63619dcfe076e2594976365421cec81
Ryan Afshar <ryanafsharryan@gmail.com>
2019-06-13 07:45:59 -0700

    Add knockback to fireballs on impact

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float duration;
+    public float speed;
+    public float strength;
+    private Vector3 direction;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, duration);
+        Debug.Log(player);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += direction * speed;
+    }
+
+    public void SetOwner(GameObject player) 
+    {
+        this.player = player;
+        Debug.Log(player);
+    }
+
+    public void SetDirection(Vector3 direction) 
+    {
+        this.direction = direction;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Vector3 difference = col.transform.position - transform.position;
+        difference.Normalize();
+        Debug.Log("difference: (for Alex)   " + difference);
+        col.gameObject.GetComponent<PlayerControls>().AddForce(difference, strength);
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public GameObject fireBall;
+    public float cooldownTime;
+    public float movementSpeed;
+    public float knockBackDuration;
+
+    private Vector2 movementVector;
+    private Vector2 aimVector;
+    private Vector2 forceDirection;
+    private float forceStrength;
+    private float forceTime; 
+    private float nextCooldown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movementVector.x = Input.GetAxis("Horizontal") * movementSpeed;
+        movementVector.y = Input.GetAxis("Vertical") * movementSpeed;
+        Vector3 velocity = movementVector * movementSpeed;
+
+        Vector3 forceVelocity = Vector3.zero;
+        if(Time.time - forceTime < knockBackDuration) {
+            forceVelocity = Vector3.Lerp(forceDirection*forceStrength, Vector3.zero, (Time.time-forceTime)/knockBackDuration);
+        }
+
+        transform.position += velocity + forceVelocity;
+
+        if (Input.GetAxis("Fire1") != 0 & (nextCooldown < Time.time))
+        {
+            // Creates an angle from the axes.
+            aimVector.x = Input.GetAxis("HorizontalTurn");
+            aimVector.y = Input.GetAxis("VerticalTurn");
+            aimVector.Normalize();
+            Debug.Log(aimVector);
+            float directionAngle = (Mathf.Atan2(aimVector.x, aimVector.y) * 180 / Mathf.PI);
+
+            Debug.Log(directionAngle);
+
+            transform.eulerAngles = new Vector3 (0f, 0f, 90 - directionAngle);
+
+            GameObject ball = Instantiate(fireBall, transform.position, Quaternion.identity) as GameObject;
+            ball.GetComponent<FireBall>().SetDirection(transform.right);
+            ball.GetComponent<FireBall>().SetOwner(this.gameObject);
+
+            nextCooldown = Time.time + cooldownTime;
+        }
+
+    }
+
+    public void AddForce(Vector3 forceDirection, float forceStrength) {
+        forceTime = Time.time;
+        this.forceDirection = forceDirection;
+        this.forceStrength = forceStrength;
+    }
+}
